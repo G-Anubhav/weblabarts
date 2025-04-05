ac122b5aa9c3a369c92818b514c2b13e371fff25
Anubhav Goyal <anubhavgoyal868@gmail.com>
2025-04-05 12:09:02 +0530

    fourth commit

--- components/home/home-eight/footer/FooterCopyright.jsx
+++ components/home/home-eight/footer/FooterCopyright.jsx
@@ -1,7 +1,7 @@
 function FooterCopyright() {
 	return (
 		<div className="sofax-footer-bottom center">
-			<p>© 2024 Made by Anubhav Goyal. All Rights Reserved.</p>
+			<p>© 2024 Made by <a href="https://github.com/G-Anubhav/">Anubhav Goyal</a>. All Rights Reserved.</p>
 		</div>
 	);
 }
